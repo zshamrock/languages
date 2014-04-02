f6ce3d913272ca247265e93d6be8fbba4ac1cde0
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-03 00:00:00 +0200

    Read fac N from stdin.

--- clojure/exercises/src/exercises/core.clj
+++ clojure/exercises/src/exercises/core.clj
@@ -2,6 +2,13 @@
   (:require [exercises.factorial :as f])  
   (:gen-class))
 
-(defn -main [& args]
-    (def n 10)
-    (println "factorials from 1 till" n "using apply-merge" (f/factorial-from-0-till-n-with-merge n)))
+(defn- get-n-or-default [n]
+    (read-string 
+        ((fnil identity "10") n)))
+
+(defn -main [& args]    
+    (let [n (get-n-or-default (first args))]
+        (println "factorials from 1 till" n "using apply-merge" (f/factorial-from-0-till-n-with-merge n))
+    ))   
+    
+
