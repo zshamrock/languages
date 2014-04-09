9ee474a878a60299c1c1280cd3ebca48ca718454
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-09 23:48:11 +0200

    Fixed indentation

--- clojure/exercises/project.clj
+++ clojure/exercises/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                          [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :main ^:skip-aot exercises.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
