314074e0a3cef8cdb61f6109d15559d2c930b78e
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-11 00:02:23 +0200

    Serving static file

--- clojure/roman-literals-web/src/roman_literals_web/core.clj
+++ clojure/roman-literals-web/src/roman_literals_web/core.clj
@@ -1,7 +1,9 @@
-(ns roman-literals-web.core)
+(ns roman-literals-web.core
+  (require [ring.util.response :refer :all]))
 
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Roman literals SPA soon must be there"})
+  (file-response "index.html" {:root "public"}))
+;  {:status 200
+;   :headers {"Content-Type" "text/html"}
+;   :body "Roman literals SPA soon must be there some changes"})
 
