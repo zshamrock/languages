ef716daa4111908185ac0e1c758e772990a6fe50
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-13 23:46:36 +0200

    Removed duplications.
    And moved serving static files into a separate function.

--- clojure/roman-literals-web/src/roman_literals_web/core.clj
+++ clojure/roman-literals-web/src/roman_literals_web/core.clj
@@ -1,19 +1,23 @@
 (ns roman-literals-web.core
-  (require [ring.util.response :refer :all]
-           [ring.util.request :refer :all]))
+  (require [ring.util.response :as response-util :refer :all]
+           [ring.util.request :as request-util :refer :all]))
+
+
+(defn- static-response
+  "Serve static files"
+  ([file] (static-response file "text/html"))
+  ([file content-type]
+    (-> (response-util/file-response file {:root "public"})
+        (response-util/content-type content-type))))
+
 
 (defn handler [request]
-  (println (path-info request))
+  (println (request-util/path-info request))
   (cond
-    (in-context? request "/js/") 
-      (-> 
-        (file-response (path-info request) {:root "public"})
-        (content-type "application/javascript"))
+    (request-util/in-context? request "/js/") 
+      (static-response (request-util/path-info request) "application/javascript")
     :else 
-      (-> 
-        (file-response "index.html" {:root "public"})
-        (content-type "text/html"))))
-;  {:status 200
+      (static-response "index.html")))
 ;   :headers {"Content-Type" "text/html"}
 ;   :body "Roman literals SPA soon must be there some changes"})
 
