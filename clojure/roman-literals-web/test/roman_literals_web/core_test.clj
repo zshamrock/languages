38bfa770a02b60c1346f9c8b174cc2ce1b3f8cb8
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-14 23:24:13 +0200

    Trying to do proper mock testing for such a simple ring app.
    Learning with-redefs.

--- clojure/roman-literals-web/test/roman_literals_web/core_test.clj
+++ clojure/roman-literals-web/test/roman_literals_web/core_test.clj
@@ -1,7 +1,15 @@
 (ns roman-literals-web.core-test
   (:require [clojure.test :refer :all]
-            [roman-literals-web.core :refer :all]))
+            [roman-literals-web.core :as core :refer :all]
+            [ring.util.request :as request-util :refer :all]
+            [ring.util.response :as response-util :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest handler-test
+  (testing "serving index.html"
+    (let [response-file (atom nil) content-type (atom nil)]
+      (with-redefs [request-util/in-context? (fn [request context] false)
+                    response-util/file-response (fn [file properties] (reset! response-file file))
+                    response-util/content-type (fn [t] (reset! content-type t))]
+        (core/handler {})
+        (is (= "index.html" @response-file))))))
+        
