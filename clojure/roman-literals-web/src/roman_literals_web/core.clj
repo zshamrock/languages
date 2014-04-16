378f292b03cf00f65b95581a412a2d270f59ebc5
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-16 22:30:12 +0200

    Removed empty and debug lines.

--- clojure/roman-literals-web/src/roman_literals_web/core.clj
+++ clojure/roman-literals-web/src/roman_literals_web/core.clj
@@ -11,12 +11,8 @@
         (response-util/content-type content-type))))
 
 (defn handler [request]
-  (println (request-util/path-info request))
   (cond
     (request-util/in-context? request "/js/") 
       (static-response (request-util/path-info request) "application/javascript")
     :else 
       (static-response "index.html")))
-;   :headers {"Content-Type" "text/html"}
-;   :body "Roman literals SPA soon must be there some changes"})
-
