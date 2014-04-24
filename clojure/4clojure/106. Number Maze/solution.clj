5e609718dedcf89f64110bf739eb4133886226bc
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-24 22:21:26 +0200

    Modified (improved) the solution of 106: Number Maze.
    Keep the track of the path from start to end, to print it when the
    answer is found. Was interesting to see that the last test produces
    different path from the one mentioned in the task. And yes there can be
    multiple paths, and it was mentioned.

--- clojure/4clojure/106. Number Maze/solution.clj
+++ clojure/4clojure/106. Number Maze/solution.clj
@@ -16,24 +16,28 @@
                     ))
           (next-values [values saw]
             (into {} 
-                  (apply concat (for [[value ops] values]
-                                  (map 
-                                    (fn [op] 
-                                      (let [next-value (op value 2)]
-                                        (when (not (contains? saw next-value))
-                                          [next-value (operations-set next-value op)]))) ops)))))]    
+                  (apply concat 
+                         (for [[value [ops path]] values]
+                           (map 
+                             (fn [op] 
+                               (let [next-value (op value 2)]
+                                 (when (not (contains? saw next-value))
+                                   [next-value [(operations-set next-value op) (conj path value)]]))) 
+                             ops)))))]
 
-    (loop [values {start (operations-set start :none)} 
-           saw #{start}
+    (loop [values {start [(operations-set start :none) []]} ; keep track of available operations as well the path
+           saw #{start} ; remember what we saw so far, in order not to do the same computation over and over again
            length 1]
       (if (contains? saw end)
-        length
+        (do
+          (let [found-path (conj (get-in values [end 1]) end)]
+            (println (str start "->" end ": " found-path))) ; path
+          length) ; length
         (let [new-values (next-values values saw)]
           (recur new-values 
                  (into saw (keys new-values)) 
                  (inc length))
           )))))
-
 
 (defn- run-all-tests []
   (is (= 1 (shortest-path 1 1))) ; 1
