d54e8600e2818f108f6cc009355133dc8e89c0ab
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-04-29 23:37:15 +0200

    Lost in recursion.
    Trying to implement the last (final) part of the algorithm, lost where
    should I exit and return either false or true. But it works, although
    returning always false :)

--- clojure/4clojure/82. Word Chains/solution.clj
+++ clojure/4clojure/82. Word Chains/solution.clj
@@ -32,8 +32,27 @@
   (is (= false (chainable? "cot" "animal")))
   (is (= false (chainable? "cot" "pos"))))
 
-(defn word-chain? [ch]
-  nil)
+(defn word-chain? [chain]
+  (let [chainable-words (all-possible-chainable-words chain)]
+    (letfn [
+            (try-to-solve [chain-so-far used-words]
+              (if (= used-words chain) 
+                true
+                (let [last-word (last chain-so-far)]
+                  (loop [possible-next-words-in-chain (get chainable-words last-word)]
+                    (if-let [next-word (first possible-next-words-in-chain)] 
+                      (when-not (contains? used-words next-word)
+                        (try-to-solve (conj chain-so-far next-word) (conj used-words next-word)))
+                      (if (next possible-next-words-in-chain)
+                        (recur (next possible-next-words-in-chain))
+                        false))))))]
+      (loop [words chain]
+        (if (seq words)
+          (if (try-to-solve [(first words)] #{(first words)})
+            true
+            (recur (next words)))
+          false)))))
+        
 
 (defn- run-all-tests []
   (is (= true (all-possible-chainable-words #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
