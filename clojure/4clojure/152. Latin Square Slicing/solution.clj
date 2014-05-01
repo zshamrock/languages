07b033bc0c86beeb4e960fea851e64966c598c62
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-05-01 23:10:05 +0200

    Added latin-square? helper fn.
    Again just the first step in the implementation. Step by step.

--- clojure/4clojure/152. Latin Square Slicing/solution.clj
+++ clojure/4clojure/152. Latin Square Slicing/solution.clj
@@ -3,6 +3,32 @@
 
 (require '[clojure.test :refer [is]])
 
+(defn- latin-square? [square] ; square is a seq where each n elements represent a row
+  (let [n (int (Math/sqrt (count square)))]
+    (letfn [
+            (rows [square]
+              (partition n square))
+
+            (cols [square] 
+              (loop [i 0 sq square cols []] ; get square cols
+                (if-not (= i n)
+                  (recur (inc i) (next sq) (conj cols (take-nth n sq)))
+                  cols)))
+
+            (unique-all? [cols-or-rows]
+              (= (* n n) (reduce + (map (comp count distinct) cols-or-rows))))]
+      (and 
+        (unique-all? (rows square))
+        (unique-all? (cols square)))))) 
+
+(defn- latin-square?-tests []
+  (is (latin-square? '[A B C B C A C A B]))
+  (is (not (latin-square? '[A B C B C A C A C])))
+  (is (latin-square? [1 2 2 1]))
+  (is (not (latin-square? [1 2 1 2]))))
+
+(latin-square?-tests)
+
 (defn latin-squares [v]
   {})
 
