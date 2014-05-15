3698078c207c7b0a03e1e79741d7cde9e8eda3f9
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-05-15 14:06:18 +0200

    A few helper functions implemented necessary to implement the idea.

--- clojure/4clojure/117. For Science!/solution.clj
+++ clojure/4clojure/117. For Science!/solution.clj
@@ -3,7 +3,42 @@
 
 (require '[clojure.test :refer [is]])
 
+(defn- normalize-maze 
+  "Transform/normalize maze from vector of strings, into vector of vectors. Elements of the inner vector are one string char (\"M\", \"C\", \" \", \"#\")."
+  [maze]
+  (mapv #(mapv str (vec %)) maze)
+  )
+
+(defn- find-what 
+  "Find coordinates [x y] of what, mouse, cheese, whatever."
+  [maze what]
+
+  (let [what (
+               loop [m maze i 0]
+               (if (some #{what} (first maze))
+                 [i (.indexOf (first maze) what)]
+                 (recur (next maze) (inc i))
+                 ))]
+    what))
+
+(defn- find-mouse 
+  "Find mouse [x y] coordinates."
+  [maze]
+  (find-what maze "M")
+  )
+
+(defn- find-cheese 
+  "Find cheese [x y] coordinates."
+  [maze]
+  (find-what maze "C")
+  )
+
 (defn cheesy-endpoint-reachable? [maze]
+  (let [m (normalize-maze maze)
+        
+        ]
+    (loop [cells [] seen #{}])
+    )
   false
   )
 
