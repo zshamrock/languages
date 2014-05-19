4409e86cb82ab59b71cedf5d8558eff299f6ccbf
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-05-20 00:41:00 +0200

    Added explanatory comments.

--- clojure/clojure-meetups/bowling-kata/src/bowling_kata/core.clj
+++ clojure/clojure-meetups/bowling-kata/src/bowling_kata/core.clj
@@ -8,13 +8,13 @@
             next-pins (fnext p) 
             nnext-pins (fnext (next p))]
         (cond 
-          (= 10 current-pins) (let [frame-score (if (and next-pins nnext-pins) (+ 10 next-pins nnext-pins) 0)]
+          (= 10 current-pins) (let [frame-score (if (and next-pins nnext-pins) (+ 10 next-pins nnext-pins) 0)] ; "if" for strike in last frame
                                 (recur (next p) (+ score frame-score))) ; strike
 
-          (= 10 (+ current-pins (or next-pins 0))) (let [frame-score (+ 10 nnext-pins)]
+          (= 10 (+ current-pins (or next-pins 0))) (let [frame-score (+ 10 nnext-pins)] ; "or" to go safely to :else part in the last frame spare 
                                                      (recur (nnext p) (+ score frame-score))) ; spare
 
-          :else (let [frame-score (if next-pins (+ current-pins next-pins) 0)] 
+          :else (let [frame-score (if next-pins (+ current-pins next-pins) 0)] ; "if" for spare in last frame
                   (recur (nnext p) (+ score frame-score)))))
       score)))
 
