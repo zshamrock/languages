6fac981f6b8b1d8aca9560047af1f6c2530ad086
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-05-19 18:15:42 +0200

    Added midje and tests to work on.

--- clojure/clojure-meetups/bowling-kata/project.clj
+++ clojure/clojure-meetups/bowling-kata/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [midje "1.6.3"]]
   :main ^:skip-aot bowling-kata.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :profiles {:dev {:dependencies [[midje "1.5.1"]]}}})
--- clojure/clojure-meetups/bowling-kata/test/bowling_kata/core_test.clj
+++ clojure/clojure-meetups/bowling-kata/test/bowling_kata/core_test.clj
@@ -1,7 +1,10 @@
 (ns bowling-kata.core-test
-  (:require [clojure.test :refer :all]
-            [bowling-kata.core :refer :all]))
+  (:use midje.sweet)
+  (:require [bowling-kata.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "bowling score"
+       (core/rolls (vec (repeat 20 0))) => 0 ; no rolls
+       (core/rolls [1 4 4 5 6 4 5 5 10 0 1 7 3 6 4 10 2 8 6]) => 133 ; sample game
+       (core/rolls [4 6 7 3 2 8 5 5 6 4 9 1 5 5 3 7 8 2 8 2 7]) => ? ; all spares
+       (core/rolls (vec (repeat 12 10))) => 300 ; perfect game
+       )
