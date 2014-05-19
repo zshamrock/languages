005113f9b211a1f74ab641ff40042ac72d66d623
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-05-19 12:45:39 +0200

    Started bowling-kata we did during one of the Clojure meetup.

---
+++ clojure/clojure-meetups/bowling-kata/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bowling-kata
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/clojure-meetups/bowling-kata/test/bowling_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns bowling-kata.core-test
+  (:require [clojure.test :refer :all]
+            [bowling-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
