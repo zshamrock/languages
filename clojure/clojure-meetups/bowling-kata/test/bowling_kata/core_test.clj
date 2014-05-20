41709c81e1629e67867a1be81340b04385aaf39d
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-05-20 11:27:08 +0200

    No vec is necessary for the facts.

--- clojure/clojure-meetups/bowling-kata/test/bowling_kata/core_test.clj
+++ clojure/clojure-meetups/bowling-kata/test/bowling_kata/core_test.clj
@@ -3,8 +3,8 @@
   (:require [bowling-kata.core :as core]))
 
 (facts "bowling score"
-       (core/rolls (vec (repeat 20 0))) => 0 ; no rolls
+       (core/rolls (repeat 20 0)) => 0 ; no rolls
        (core/rolls [1 4 4 5 6 4 5 5 10 0 1 7 3 6 4 10 2 8 6]) => 133 ; sample game
        (core/rolls [4 6 7 3 2 8 5 5 6 4 9 1 5 5 3 7 8 2 8 2 7]) => 160 ; all spares
-       (core/rolls (vec (repeat 12 10))) => 300 ; perfect game
+       (core/rolls (repeat 12 10)) => 300 ; perfect game
        )
