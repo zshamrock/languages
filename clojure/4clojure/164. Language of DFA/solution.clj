18645454502603aec81f7cdcdff799e7be58d2e2
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-06-02 16:08:47 +0200

    TDD on pr. 164: Language of a DFA.

--- clojure/4clojure/164. Language of DFA/solution.clj
+++ clojure/4clojure/164. Language of DFA/solution.clj
@@ -5,8 +5,20 @@
 
 (require '[clojure.test :refer [is]])
 
+(defn recognized-strings [dfa]
+  #{} 
+  )
+
 (defn- run-all-test []
-  
+  (is (= #{"a" "ab" "abc"}
+         (set  (recognized-strings 
+                 '{:states #{q0 q1 q2 q3}
+                   :alphabet #{a b c}
+                   :start q0
+                   :accepts #{q1 q2 q3}
+                   :transitions  {q0  {a q1}
+                                  q1  {b q2}
+                                  q2  {c q3}}}))))
   )
 
 (run-all-test)
