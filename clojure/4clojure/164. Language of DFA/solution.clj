d216c2a90f9b66c5175299c5b120746e21813a08
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-06-03 14:33:51 +0200

    Working on the infinite test case.
    Probably need to read some theory on automata, of course, can complete
    this test without reading, but reading will make the solution more
    solid.

--- clojure/4clojure/164. Language of DFA/solution.clj
+++ clojure/4clojure/164. Language of DFA/solution.clj
@@ -5,19 +5,19 @@
 
 (require '[clojure.test :refer [is]])
 
-(defn- traverse [transitions seen-states state strings string dfa]
+(defn- traverse [transitions state seen-states strings string dfa]
   (if (seq transitions)
-    (apply clojure.set/union 
+    (apply concat 
            (for [[state-value new-state] (vec transitions) 
-                 :when (not (contains? seen-states new-state))
+                 :when (and (not (contains? seen-states state)) (not= new-state state))
                  :let [new-string (str string state-value)]]
              (do
                (comment (println state-value new-state))
 
                (traverse 
                  (get (:transitions dfa) new-state) 
-                 (conj seen-states new-state) 
                  new-state 
+                 (conj seen-states state)
                  (if (contains? (:accepts dfa) new-state) (conj strings new-string) strings) 
                  new-string 
                  dfa))))  
@@ -25,7 +25,7 @@
   )
 
 (defn recognized-strings [{:keys [states alphabet start accepts transitions] :as dfa}]
-  (lazy-seq (traverse (get transitions start) #{start} start [] "" dfa)))
+  (traverse (get transitions start) start #{} [] "" dfa))
 
 (defn- run-all-test []
   (is (= #{"a" "ab" "abc"}
@@ -61,7 +61,7 @@
                                 q2 {v q3, w q3, x q3, y q3, z q3}
                                 q3 {v q4, w q4, x q4, y q4, z q4}}}))))
 
-  (let [res (take 2000 (recognized-strings 
+  (let [res (take 10 (recognized-strings 
                          '{:states #{q0 q1}
                            :alphabet #{0 1}
                            :start q0
