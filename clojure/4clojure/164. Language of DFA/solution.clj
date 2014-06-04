106bcfdaf743617481272d994c17c5f6edb7d5a8
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-06-04 16:34:16 +0200

    Solved problem 164, although (take X) always process on a limited list.
    I don't return the infinite list from the (traverse), but tests expect
    it to be infinite? Interesting to see other solutions, to learn new
    Clojure construct.

--- clojure/4clojure/164. Language of DFA/solution.clj
+++ clojure/4clojure/164. Language of DFA/solution.clj
@@ -6,22 +6,27 @@
 (require '[clojure.test :refer [is]])
 
 (defn- traverse [transitions state seen-states strings string dfa]
-  (if (seq transitions)
-    (apply concat 
-           (for [[state-value new-state] (vec transitions) 
-                 :when (and (not (contains? seen-states state)) (not= new-state state))
-                 :let [new-string (str string state-value)]]
-             (do
-               (comment (println state-value new-state))
+  (comment (println "seen states:" seen-states ", state:" state ", strings: " strings))
+  (if (and (= seen-states (:states dfa)) (contains? (:accepts dfa) state))
+    strings 
 
-               (traverse 
-                 (get (:transitions dfa) new-state) 
-                 new-state 
-                 (conj seen-states state)
-                 (if (contains? (:accepts dfa) new-state) (conj strings new-string) strings) 
-                 new-string 
-                 dfa))))  
-    strings)
+    (if (seq transitions)
+      (apply concat 
+             (for [[state-value new-state] (vec transitions) 
+                   :when (and (not (contains? seen-states state)))
+                   :let [new-string (str string state-value)]]
+               (do
+                 (comment (println state-value new-state))
+
+                 (traverse 
+                   (get (:transitions dfa) new-state) 
+                   new-state 
+                   (conj seen-states state)
+                   (if (contains? (:accepts dfa) new-state) (conj strings new-string) strings) 
+                   new-string 
+                   dfa))) 
+             )  
+      strings))
   )
 
 (defn recognized-strings [{:keys [states alphabet start accepts transitions] :as dfa}]
@@ -30,13 +35,13 @@
 (defn- run-all-test []
   (is (= #{"a" "ab" "abc"}
          (set (recognized-strings 
-                 '{:states #{q0 q1 q2 q3}
-                   :alphabet #{a b c}
-                   :start q0
-                   :accepts #{q1 q2 q3}
-                   :transitions  {q0  {a q1}
-                                  q1  {b q2}
-                                  q2  {c q3}}}))))
+                '{:states #{q0 q1 q2 q3}
+                  :alphabet #{a b c}
+                  :start q0
+                  :accepts #{q1 q2 q3}
+                  :transitions  {q0  {a q1}
+                                 q1  {b q2}
+                                 q2  {c q3}}}))))
 
   (is (= #{"hi" "hey" "hello"}
          (set (recognized-strings 
@@ -61,7 +66,7 @@
                                 q2 {v q3, w q3, x q3, y q3, z q3}
                                 q3 {v q4, w q4, x q4, y q4, z q4}}}))))
 
-  (let [res (take 10 (recognized-strings 
+  (let [res (take 2000 (recognized-strings 
                          '{:states #{q0 q1}
                            :alphabet #{0 1}
                            :start q0
@@ -70,6 +75,32 @@
                                          q1 {0 q1, 1 q0}}}))]
     (is (and (every? (partial re-matches #"0*(?:10*10*)*") res)
              (= res (distinct res)))))
+
+  (let  [res  (take 2000 (recognized-strings 
+                           '{:states #{q0 q1}
+                             :alphabet #{n m}
+                             :start q0
+                             :accepts #{q1}
+                             :transitions  {q0  {n q0, m q1}}}))]
+    (is (and  (every?  (partial re-matches #"n*m") res)
+             (= res  (distinct res)))))
+
+  (let  [res  (take 2000 (recognized-strings 
+                           '{:states #{q0 q1 q2 q3 q4 q5 q6 q7 q8 q9}
+                             :alphabet #{i l o m p t}
+                             :start q0
+                             :accepts #{q5 q8}
+                             :transitions  {q0  {l q1}
+                                            q1  {i q2, o q6}
+                                            q2  {m q3}
+                                            q3  {i q4}
+                                            q4  {t q5}
+                                            q6  {o q7}
+                                            q7  {p q8}
+                                            q8  {l q9}
+                                            q9  {o q6}}}))]
+    (is (and  (every?  (partial re-matches #"limit|(?:loop)+") res)
+             (= res  (distinct res)))))
   )
 
 (run-all-test)
