98fa0b8ed6b121d026896e68e80704b53ace1293
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-06-15 23:12:50 +0200

    Prepare for problem 91: Graph Connectivity.

---
+++ clojure/4clojure/91. Graph Connectivity/solution.clj
@@ -0,0 +1,30 @@
+; 91. Graph Connectivity 
+; https://www.4clojure.com/problem/91
+
+(in-ns 'user)
+
+(require '[clojure.test :refer [is]])
+
+(defn connected? [graph]
+  false
+  )
+
+(defn- run-all-tests  []
+  (is (= true  (connected? #{[:a :a]})))
+
+  (is (= true  (connected? #{[:a :b]})))
+
+  (is (= false  (connected? #{[1 2]  [2 3]  [3 1]
+                              [4 5]  [5 6]  [6 4]})))
+
+  (is (= true  (connected? #{[1 2]  [2 3]  [3 1]
+                             [4 5]  [5 6]  [6 4]  [3 4]})))
+
+  (is (= false  (connected? #{[:a :b]  [:b :c]  [:c :d]
+                              [:x :y]  [:d :a]  [:b :e]})))
+
+  (is (= true  (connected? #{[:a :b]  [:b :c]  [:c :d]
+                             [:x :y]  [:d :a]  [:b :e]  [:x :a]})))
+  )
+
+(run-all-tests)
