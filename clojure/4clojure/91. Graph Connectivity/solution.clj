cba5029164a352587c1d870f9316ce8b15a1de9a
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-06-16 17:56:58 +0200

    Do convert graph (set of tuples into proper map).
    The resulting map represents accessible nodes from the others, like the
    way you draw the graph.

--- clojure/4clojure/91. Graph Connectivity/solution.clj
+++ clojure/4clojure/91. Graph Connectivity/solution.clj
@@ -6,26 +6,26 @@
 (require '[clojure.test :refer [is]])
 
 (defn connected? [graph]
-  (let [g (apply merge-with vector (map #(conj {} % (vec (reverse %))) graph))]
+  (let [g (apply merge-with #(vec (concat %1 %2)) (map #(conj {} [(first %) [(second %)]] [(second %) [(first %)]]) graph))]
     g
     )
   )
 
 (defn- run-all-tests  []
-  (is (= true  (connected? #{[:a :a]})))
+  (is (= true (connected? #{[:a :a]})))
 
-  (is (= true  (connected? #{[:a :b]})))
+  (is (= true (connected? #{[:a :b]})))
 
-  (is (= false  (connected? #{[1 2]  [2 3]  [3 1]
+  (is (= false (connected? #{[1 2]  [2 3]  [3 1]
                               [4 5]  [5 6]  [6 4]})))
 
-  (is (= true  (connected? #{[1 2]  [2 3]  [3 1]
+  (is (= true (connected? #{[1 2]  [2 3]  [3 1]
                              [4 5]  [5 6]  [6 4]  [3 4]})))
 
-  (is (= false  (connected? #{[:a :b]  [:b :c]  [:c :d]
+  (is (= false (connected? #{[:a :b]  [:b :c]  [:c :d]
                               [:x :y]  [:d :a]  [:b :e]})))
 
-  (is (= true  (connected? #{[:a :b]  [:b :c]  [:c :d]
+  (is (= true (connected? #{[:a :b]  [:b :c]  [:c :d]
                              [:x :y]  [:d :a]  [:b :e]  [:x :a]})))
   )
 
