1a1da2490c8ea0403d138b9fccd393c9ca5885e3
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-07-01 17:27:33 +0200

    Workbench - experiment on different language features.

---
+++ clojure/workbench.clj
@@ -0,0 +1,11 @@
+(in-ns 'user)
+
+; defrecord-s
+(defrecord Book [title author])
+
+(let [reading (map->Book {:title "Волонтеры вечности" :author "Макс Фрай"})
+      read (->Book "Чужак" "Макс Фрай")
+      books [reading read]
+      titles (map :title (filter (fn  [book]  (=  (:author book) "Макс Фрай")) books))]
+  (println titles)
+  )
