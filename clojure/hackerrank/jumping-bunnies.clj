4909d97cf7319ca9de6230758f177702ab00dc55
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-07-14 01:01:13 +0200

    Solved jumping-bunnies. Could use gcd to calc lcd, but used prime factorization instead.

--- clojure/hackerrank/jumping-bunnies.clj
+++ clojure/hackerrank/jumping-bunnies.clj
@@ -9,25 +9,26 @@
                              (recur (+ solution longest-jump))))))]
   (println (find-solution jumps)))
 
+
 (defn prime-factorization [n]
   (let [primes (loop [number n primes []]
-                 (let [next-prime (first (filter #(zero? (rem number %)) [2 3 5 7]))]
+                 (let [next-prime (first (filter #(zero? (rem number %)) [2 3 5 7 11]))]
                    (if next-prime
                      (recur (/ number next-prime) (conj primes next-prime))
                      (if (= number 1) primes 
                        (conj primes number)))))]
-    (sort primes)))
+    primes))
 
 (defn count-occurences [coll]
   (reduce #(assoc %1 %2 (inc (%1 %2 0))) {} coll)
   )
 
 
-(let [all-factorization (for [n [2 6 4]]
-                          (count-occurences (prime-factorization n))
-                          )]
-  (long (reduce #(* %1 (Math/pow (key %2) (val %2))) 1 (apply merge-with max all-factorization)))
-  )
+(let [all-factorization (for [n [597 322 187 734 498 215 176 451 114 204]]
+                          (count-occurences (prime-factorization n)))
+      lcm (apply merge-with max all-factorization)]
+  (println lcm)
+  (long (reduce #(* %1 (long (Math/pow (key %2) (val %2)))) 1 lcm)))
 
 ; different ways to do count of elements in the collection
 (let [coll [2 2 3 4 2 5 5]] 
@@ -40,3 +41,21 @@
 
 (merge-with + {:a 12} {:b 4} {:a 3 :b 7})
 
+; solution formatted for hackerrank
+(letfn [(count-occurences [coll]
+          (reduce #(assoc %1 %2 (inc (%1 %2 0))) {} coll))
+
+        (prime-factorization [n]
+          (let [primes (loop [number n primes []]
+                         (let [next-prime (first (filter #(zero? (rem number %)) [2 3 5 7 11]))]
+                           (if next-prime
+                             (recur (/ number next-prime) (conj primes next-prime))
+                             (if (= number 1) primes 
+                               (conj primes number)))))]
+            (sort primes)))]
+  (let [_ (read-line)
+        jumps (map #(Long/parseLong %) (clojure.string/split  (read-line) #" "))
+        all-factorization (for [n jumps]
+                            (count-occurences (prime-factorization n)))
+        lcm (apply merge-with max all-factorization)]
+    (println (long (reduce #(* %1 (long (Math/pow (key %2) (val %2)))) 1 lcm)))))
