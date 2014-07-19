bd58c44834ddbdf154f3c20c490a36ab128cbf66
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-07-20 00:52:40 +0200

    Solved pr. 101: Levenshtein Distance. Learnt quite nice trick with (memoize) function (either partial or apply twice).

--- clojure/4clojure/101. Levenshtein Distance/solution.clj
+++ clojure/4clojure/101. Levenshtein Distance/solution.clj
@@ -5,7 +5,7 @@
 
 (require '[clojure.test :refer [is]])
 
-(defn levenshtein-distance [x y]
+(defn -levenshtein-distance [x y]
   (let [cx (count x) 
         cy (count y)
         distance (fn [x y]
@@ -38,6 +38,46 @@
 (is (= ["li" "ls" "lp" "is" "ip" "sp"] (generate "lisp" 2)))
 (is (= ["lis" "lip" "lsp" "isp"] (generate "lisp" 3)))
 (is (= ["lisp"] (generate "lisp" 4)))
+
+(def lev
+  (memoize (fn [a b i j]
+             (cond 
+               (zero? (min i j)) 
+               (max i j)
+               
+               :default
+               (min (+ (lev a b (dec i) j) 1) 
+                    (+ (lev a b i (dec j)) 1) 
+                    (+ (lev a b (dec i) (dec j)) (if (= (get a (dec i)) (get b (dec j))) 0 1))
+                    )
+               )
+            
+            ))
+  )
+
+(is (= (lev "kitten" "sitting" (count "kitten") (count "sitting")) 3))
+
+; final solution, actually because 4clojure doesn't accept (def) to declare memoize recursive function inside let (partial lev lev) trick is used
+; another approach is to declare fn (f (fn [s a b) ...)), then define memoize (g (memoize f)) and apply g to f (g g a b), so actually s becomes g, which is memoized f :)
+(defn levenshtein-distance [a b]
+  (let [
+        lev (memoize (fn [f a b]
+                       (cond 
+                         (zero? (min (count a) (count b))) 
+                         (max (count a) (count b))
+
+                         :default
+                         (min (+ (f f (rest a) b) 1) 
+                              (+ (f f a (rest b)) 1) 
+                              (+ (f f (rest a) (rest b)) (if (= (first a) (first b)) 0 1))
+                              )
+                         )))
+
+
+        ]
+    ((partial lev lev) a b) 
+    )
+  )
 
 
 (defn- run-all-tests []
