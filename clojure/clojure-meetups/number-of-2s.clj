69c6bb738f4f2910fcddfe5ae3ae3964ee3f4a63
Aliaksandr Kazlou <alex.kazlou@booking.com>
2014-07-21 11:06:15 +0200

    Operate on a list of numbers, instead of chars.

--- clojure/clojure-meetups/number-of-2s.clj
+++ clojure/clojure-meetups/number-of-2s.clj
@@ -4,9 +4,9 @@
 
 ; counts the number of '2' digits between 0 and n
 (defn number-of-2s [number]
-  (let [all-tenth (loop [n (str number) cnt 0]
+  (let [all-tenth (loop [n (map #(-> % str Integer/parseInt) (str number)) cnt 0]
                     (if (seq n) 
-                      (let [head (Integer/parseInt (str (first n))) 
+                      (let [head (first n) 
                             pos (count n)
                             ]
                         (cond
@@ -27,13 +27,15 @@
     )
   )
 
-(is (= (number-of-2s 25) 9))
-(is (= (number-of-2s 1) 0))
-(is (= (number-of-2s 2) 1))
-(is (= (number-of-2s 0) 0))
-(is (= (number-of-2s 6) 1))
-(is (= (number-of-2s 10) 1))
-(is (= (number-of-2s 11) 1))
-(is (= (number-of-2s 12) 2))
-(is (= (number-of-2s 125) 19))
+(defn- run-all-tests [] 
+  (is (= (number-of-2s 25) 9))
+  (is (= (number-of-2s 1) 0))
+  (is (= (number-of-2s 2) 1))
+  (is (= (number-of-2s 0) 0))
+  (is (= (number-of-2s 6) 1))
+  (is (= (number-of-2s 10) 1))
+  (is (= (number-of-2s 11) 1))
+  (is (= (number-of-2s 12) 2))
+  (is (= (number-of-2s 125) 19)))
 
+(run-all-tests)
