87ef2c2bce25a4511098c907eeb153b320ee65be
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-07-26 23:02:13 +0200

    Solved pr. 177: Balancing Brackets.

---
+++ clojure/4clojure/177. Balancing Brackets/solution.clj
@@ -0,0 +1,47 @@
+; 177. Balancing Brackets
+; https://www.4clojure.com/problem/177
+(in-ns 'user)
+
+(require '[clojure.test :refer [is]])
+
+(defn balanced? [c]
+  (let [open-brackets "([{"
+        close-brackets ")]}"
+        brackets-mapping (into {} (map (fn [open-bracket close-bracket] [close-bracket open-bracket]) open-brackets close-brackets))
+        ]
+    (loop [brackets []
+           code c]
+
+      (if (seq code)
+
+        (let [ch (first code)]
+          (cond 
+            (some #{ch} "[({") ; open bracket
+            (recur (conj brackets ch) (rest code))
+
+            (some #{ch} "])}") ; close bracket
+            (when (= (get brackets-mapping ch) (peek brackets))
+              (recur (pop brackets) (rest code))
+              )
+
+            :default
+            (recur brackets (rest code))
+            ))
+
+        (empty? brackets)))))
+
+(defn- run-all-tests []
+  (is (balanced? "This string has no brackets."))
+  (is (balanced? "class Test {
+            public static void main(String[] args) {
+              System.out.println(\"Hello world.\");
+            }}"))
+  (is (not (balanced? "(start, end]")))
+  (is (not (balanced? "())")))
+  (is (not (balanced? "[ { ] } ")))
+  (is (balanced? "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))"))
+  (is (not (balanced? "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))")))
+  (is (not (balanced? "[")))
+  )
+
+(run-all-tests)
