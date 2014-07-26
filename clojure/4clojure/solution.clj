a0aff083b1007a3f896c170d181b322278ee2877
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-07-26 22:11:26 +0200

    Added solution starter template (finally :)

---
+++ clojure/4clojure/solution.clj
@@ -0,0 +1,14 @@
+; NNN. Problem name 
+; https://www.4clojure.com/problem/nnn
+
+(require '[clojure.test :refer [is]])
+
+(defn solution []
+  nil
+  )
+
+(defn- run-all-tests []
+  
+  )
+
+(run-all-tests)
