cbef1d766efae41254eb6541a6190a4ade93ffe2
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-08-05 23:46:10 +0200

    Space.

--- python/generate-marketing-email.py
+++ python/generate-marketing-email.py
@@ -5,6 +5,6 @@ from Cheetah.Template import Template
 
 mail = Template(file="mail.tmpl")
 mail.title = u"Зима близко!"
-mail.recipients=["Rob", "Bran", "John"]
+mail.recipients = ["Rob", "Bran", "John"]
 
 print mail
